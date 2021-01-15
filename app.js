004c59dfe29a956554c4ee6396d3c6227ab1c979
Your Name <you@example.com>
2021-01-15 21:37:19 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const config =  require('config')
+
+const app = express()
+ 
+const PORT = config.get('port') || 5000
+
+app.use
+
+async function start () {
+    try {
+        app.listen(5000, () => console.log(`App has been started on port ${PORT}... `))
+    } catch (e) {
+        console.log('Server Error', e.message)
+        process.exit
+    }
+}
+
+start()
